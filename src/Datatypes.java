c3e66aa58abaacdfe48438e0e64b1c6288cedfb4
Slainez <sullyvan360@gmail.com>
2022-07-05 12:22:59 +0200

    add datatypes

---
+++ src/Datatypes.java
@@ -0,0 +1,21 @@
+public class Datatypes {
+    public static void main(String[] args) {
+//      Chaîne de caractères
+        String prenom = "Sullyvan";
+        String nom = "LAINEZ";
+
+//    Caractères
+    char genre = 'M';
+
+
+//   Réels
+    float poids = 75.5F;
+
+//   Réels double precision
+
+    double    pi = 3.7682526655225565;
+
+//      Byte : variante d'entier court
+    byte age = 26 ;
+    }
+}
