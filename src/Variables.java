2db97b1ecd53c2ad194ec2ff700972edc491c82f
Slainez <sullyvan360@gmail.com>
2022-07-05 11:27:28 +0200

    add some compute

---
+++ src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main (String[] args) {
+        int longueur = 200;
+    //    longueur = 20;
+    //    longueur = 35;
+    
+        int largeur = 50;
+     //   largeur=20;
+    //    largeur=10;
+
+
+        int perimetre = 2*(longueur+largeur);
+        int surface = longueur*largeur;
+
+        System.out.println(perimetre);
+        System.out.println(surface);
+    }
+}
